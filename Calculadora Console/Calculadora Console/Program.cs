a9cdd6b961d5bb149156a6627134ac61c2ae0aa9
Cassio Gama <157303477+Cassio-Gama@users.noreply.github.com>
2024-03-15 09:32:50 -0300

    Add files via upload

---
+++ Calculadora Console/Calculadora Console/Program.cs
@@ -0,0 +1,91 @@
+﻿
+
+int w = 1;
+
+while( w != 0){
+
+    Console.WriteLine("Calculadora - Console");
+    Console.WriteLine("1) Soma");
+    Console.WriteLine("2) Subtração");
+    Console.WriteLine("3) Divisão");
+    Console.WriteLine("4) Multiplicação");
+    Console.WriteLine("5) Sair");
+    int op = int.Parse(Console.ReadLine());
+
+    if(op == 1)
+    {
+        soma();
+    }
+    else if(op == 2)
+    {
+        subtracao();
+    }
+    else if(op == 3)
+    {
+        divisao();
+    }
+    else if(op == 4)
+    {
+        multiplicacao();
+    }
+    else
+    { 
+        Console.WriteLine("Até Logo!");
+        break;
+    }
+    
+
+}
+
+
+void soma()
+{
+    Console.WriteLine("Opção - Soma");
+    Console.WriteLine("Insira um número: ");
+    double num1 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Insira outro número");
+    double num2 = double.Parse(Console.ReadLine());
+
+    double resultado = num1 + num2;
+
+    Console.WriteLine("Resultado: " + resultado);
+}
+
+void subtracao()
+{
+    Console.WriteLine("Opção - Subtracao");
+    Console.WriteLine("Insira um número: ");
+    double num1 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Insira outro número");
+    double num2 = double.Parse(Console.ReadLine());
+
+    double resultado = num1 - num2;
+
+    Console.WriteLine("Resultado: " + resultado);
+}
+
+void divisao()
+{
+    Console.WriteLine("Opção - Divisão");
+    Console.WriteLine("Insira um número: ");
+    double num1 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Insira outro número");
+    double num2 = double.Parse(Console.ReadLine());
+
+    double resultado = num1 / num2;
+
+    Console.WriteLine("Resultado: " + resultado);
+}
+
+void multiplicacao()
+{
+    Console.WriteLine("Opção - Multiplicação");
+    Console.WriteLine("Insira um número: ");
+    double num1 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Insira outro número");
+    double num2 = double.Parse(Console.ReadLine());
+
+    double resultado = num1 * num2;
+
+    Console.WriteLine("Resultado: " + resultado);
+}
